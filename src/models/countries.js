d210f95f4f48c12c20b78af215977ce2f5c19753
Callum Anderson <callum_anderson@hotmail.com>
2018-09-12 12:28:11 +0100

    JSON request set up

---
+++ src/models/countries.js
@@ -0,0 +1,25 @@
+const Request = require('../helpers/request.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+
+const Countries = function() {
+  this.info = [];
+};
+Countries.prototype.getData = function () {
+  const request = new Request('https://restcountries.eu/rest/v2/all');
+
+  request.get((data) => {
+    this.info = data.map(country => {
+      let rCountry = {};
+      rCountry['name'] = country['name'];
+      rCountry['region'] = country['region'];
+      rCountry['flag'] = country['flag'];
+      return rCountry;
+    });
+    console.log(this.info);
+    PubSub.publish('Countries:countries-loaded', this.info);
+
+  });
+};
+
+module.exports = Countries;
